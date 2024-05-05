76290ac86526ab206fc4ce9389c5fdfbac7134d1
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-05 08:13:32 +0100

    initial commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+0. Simple helper function
+'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculates the start and end indexes for a given page and page size.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end indexes (inclusive).
+    """
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
