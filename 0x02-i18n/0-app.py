d247b26789fe6101f5d77331f89cdbef5de37d9b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-14 12:56:26 +0100

    initial commit

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+0. Basic Flask app
+'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def basic():
+    '''
+    Basic app that displays template messages
+    under 'title' annd 'h1' tags 
+    '''
+    tittle = "Welcome to Holberton"
+    message = "Hello world"
+
+    return render_template("0-index.html", tittle=tittle, message=message)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
